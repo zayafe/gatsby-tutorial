14fd8efd05e97de63b9ec38d3a53cb43320c7903
Cynthia Voss <cynthia.voss98@gmail.com>
2021-06-16 17:12:13 +0200

    Initial commit

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e0facf4e96dc5cd18bc06fc3138ad484'),
